2350ee58c7949ee49eaee49349909b42288a0738
kurz-m <m.kurz92@gmail.com>
2023-04-04 20:54:52 +0200

    added the last to tests. Possible failure because of os.

--- tests/Xx_tests.c
+++ tests/Xx_tests.c
@@ -6,7 +6,7 @@
 /*   By: makurz <makurz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:07:35 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/04 17:48:37 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/04 20:53:58 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ void setup(void)
 
 TestSuite(X_test, .init=setup);
 
-Test(X_test, u_basic_1)
+Test(X_test, X_basic_1)
 {
 	char	buf[70];
 	bzero(buf, 70);
 	setbuf(stderr, buf);
-	fprintf(stderr, "testing %u %u %u", (unsigned int) -34, 53, 53);
+	fprintf(stderr, "testing %X %X %X", 0, 53, 53);
 	setbuf(stderr, NULL);
-	ft_printf("testing %u %u %u", (unsigned int) -34, 53, 53);
+	ft_printf("testing %X %X %X", 0, 53, 53);
 	cr_expect_stdout_eq_str(buf);
 }
 
-Test(X_test, u_basic_2)
+Test(X_test, X_basic_2)
 {
 	char	buf[70];
 	bzero(buf, 70);
@@ -47,92 +47,80 @@ Test(X_test, u_basic_2)
 	cr_expect_stdout_eq_str(buf);
 }
 
-Test(X_test, u_basic_3)
+Test(X_test, X_basic_3)
 {
 	char	buf[70];
 	bzero(buf, 70);
 	setbuf(stderr, buf);
-	fprintf(stderr, "%u", (unsigned int) INT_MAX);
+	fprintf(stderr, "%lX", ULONG_MAX);
 	setbuf(stderr, NULL);
-	ft_printf("%u", (unsigned int) INT_MAX);
+	ft_printf("%X", (unsigned int) ULONG_MAX);
 	cr_expect_stdout_eq_str(buf);
 }
 
-Test(X_test, u_basic_4)
+Test(X_test, X_basic_4)
 {
 	char	buf[70];
 	bzero(buf, 70);
 	setbuf(stderr, buf);
-	fprintf(stderr, "%u%u%u%u", 0, 0, 0, 0);
+	fprintf(stderr, "%X%X%lX%lX", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN);
 	setbuf(stderr, NULL);
-	ft_printf("%u%u%u%u", 0, 0, 0, 0);
+	ft_printf("%X%X%X%X", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN);
 	cr_expect_stdout_eq_str(buf);
 }
-
 TestSuite(x_test, .init=setup);
 
-Test(x_test, u_basic_1)
+Test(x_test, x_basic_1)
 {
 	char	buf[70];
 	bzero(buf, 70);
 	setbuf(stderr, buf);
-	fprintf(stderr, "testing %u %u %u", (unsigned int) -34, 53, 53);
+	fprintf(stderr, "testing %x %x %x", INT_MIN, INT_MAX, INT_MAX);
 	setbuf(stderr, NULL);
-	ft_printf("testing %u %u %u", (unsigned int) -34, 53, 53);
+	ft_printf("testing %x %x %x", INT_MIN, INT_MAX, INT_MAX);
 	cr_expect_stdout_eq_str(buf);
 }
 
-Test(x_test, u_basic_2)
+Test(x_test, x_basic_2)
 {
 	char	buf[70];
 	bzero(buf, 70);
 	setbuf(stderr, buf);
-	fprintf(stderr, "%u", UINT_MAX);
+	fprintf(stderr, "%x", UINT_MAX);
 	setbuf(stderr, NULL);
-	ft_printf("%u", UINT_MAX);
+	ft_printf("%x", UINT_MAX);
 	cr_expect_stdout_eq_str(buf);
 }
 
-Test(x_test, u_basic_3)
+Test(x_test, x_basic_3)
 {
 	char	buf[70];
 	bzero(buf, 70);
 	setbuf(stderr, buf);
-	fprintf(stderr, "%u", (unsigned int) INT_MAX);
+	fprintf(stderr, "%x", 0);
 	setbuf(stderr, NULL);
-	ft_printf("%u", (unsigned int) INT_MAX);
+	ft_printf("%x", 0);
 	cr_expect_stdout_eq_str(buf);
 }
 
-Test(x_test, u_basic_4)
+Test(x_test, x_basic_4)
 {
 	char	buf[70];
 	bzero(buf, 70);
 	setbuf(stderr, buf);
-	fprintf(stderr, "%u%u%u%u", 0, 0, 0, 0);
+	fprintf(stderr, "%x%x%x%x", 0, 0, 0, 0);
 	setbuf(stderr, NULL);
-	ft_printf("%u%u%u%u", 0, 0, 0, 0);
+	ft_printf("%x%x%x%x", 0, 0, 0, 0);
 	cr_expect_stdout_eq_str(buf);
 }
 
-Test(x_test, u_basic_5)
+Test(x_test, x_basic_5)
 {
 	char	buf[70];
 	bzero(buf, 70);
 	setbuf(stderr, buf);
-	fprintf(stderr, "Ordinary string nb: %u", -532342);
+	fprintf(stderr, "Ordinary string nb: %x", -532342);
 	setbuf(stderr, NULL);
-	ft_printf("Ordinary string nb: %u", -532342);
-	cr_expect_stdout_eq_str(buf);
-}
-
-Test(x_test, u_basic_6)
-{
-	char	buf[70];
-	bzero(buf, 70);
-	setbuf(stderr, buf);
-	fprintf(stderr, "%% %u from %% %u", 34241, -43241);
-	setbuf(stderr, NULL);
-	ft_printf("%% %u from %% %u", 34241, -43241);
+	ft_printf("Ordinary string nb: %x", -532342);
 	cr_expect_stdout_eq_str(buf);
 }
